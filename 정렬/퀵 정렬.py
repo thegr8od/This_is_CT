810aa885452305651b74e544d23de207ddf8fcf8
Seungjun Kim <zzjoon97@naver.com>
2024-02-14 14:17:16 +0900

    240214

---
+++ 정렬/퀵 정렬.py
@@ -0,0 +1,13 @@
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array, start, end):
+    if start >= end: #원소가 1개인 경우 종료
+        return
+    pivot = start # 피벗은 첫 번째 원소
+    left = start + 1
+    right = end
+    while left <= right:
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+    

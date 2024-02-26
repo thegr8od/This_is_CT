364a9f9283f7927762b4a6a93b7e3350a1cbeb5a
Seungjun Kim <zzjoon97@naver.com>
2024-02-26 17:34:09 +0900

    240226

--- 최단 경로/다익스트라(우선순위 큐).py
+++ 최단 경로/다익스트라(우선순위 큐).py
@@ -6,3 +6,10 @@ INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 # 노드의 개수, 간선의 개수를 입력받기
 n, m = map(int, input().split())
 # 시작 노드 번호를 입력받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[]for i in range(n+1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n+1)
+
+#
